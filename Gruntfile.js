99875da94c67f0acb5321106ab4943aa6af0ada5
amirsher <locosher@gmail.com>
2015-10-03 15:39:50 +0300

    # upload from local #

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ grunt.loadNpmTasks('grunt-text-replace');
 
 
 // Default task(s).
-  grunt.registerTask('default', ["shell:jekyllBuild", "replace", "connect", "watch"]);
+  grunt.registerTask('default', ["shell:jekyllBuild", "connect", "watch"]);
   grunt.registerTask('html', ['htmlmin']);
   grunt.registerTask('css', ['cssmin']);
   grunt.registerTask("serve", ["shell:jekyllServe"]);
