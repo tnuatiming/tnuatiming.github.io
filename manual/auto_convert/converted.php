b9f1f6945e9814c27ad2d33b18064f742c0c695d
amirsher <locosher@gmail.com>
2016-10-31 20:30:07 +0200

    # upload from local #

--- manual/auto_convert/converted.php
+++ manual/auto_convert/converted.php
@@ -1,15 +1,16 @@
- <?php
+<?php
 $csv_file = file_get_contents($_FILES['file']['tmp_name']);
 $delimiter = array("\t",",","|","\\","/",";");
 $tmp = str_replace($delimiter, $delimiter[1], $csv_file);// convert delimiter to ,
-$csv = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $tmp);//delete empty lines
+//$csv = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $tmp);//delete empty lines
+$csv = preg_replace('/^[\r\n]+|[\r\n]+$/m', '', $tmp);//delete empty lines
 function convert($csv)
 {
   $out = [];
   array_map(function($ln) use(&$out) {
       $ln    = htmlentities($ln);
       $out[] = count($out) == 0
-        ? "\t<tr class=\"rnkh_bkcolor\">\n\t\t<th>".implode("</th>\n\t\t<th>",explode(",",$ln))."</th>\n\t</tr>\n"
+        ? "\t<tr class=\"rnkh_bkcolor\">\n\t\t<th class=\"rnkh_font\">".implode("</th>\n\t\t<th class=\"rnkh_font\">",explode(",",$ln))."</th>\n\t</tr>\n"
         : "\t<tr class=\"rnk_bkcolor\">\n\t\t<td class=\"rnk_font\">".implode("</td>\n\t\t<td class=\"rnk_font\">",explode(",",$ln))."</td>\n\t</tr>\n";
     }, explode("\n",$csv));
   return "\n<table cellspacing=1 class=\"line_color\">\n".implode('',$out)."</table>\n";
