51b0d81408acbca1616b0b7905496e169d2877ab
amirsher <locosher@gmail.com>
2016-10-31 18:28:14 +0200

    # upload from local #

--- manual/auto_convert/converted.php
+++ manual/auto_convert/converted.php
@@ -1,9 +1,9 @@
  <?php
-$csv = file_get_contents($_FILES['file']['tmp_name']);
-$delimiter = array("\t",",","'","\"","|","\\","/",";");
-$replace1 = str_replace($delimiter, $delimiter[1], $csv);// convert delimiter to ,
-$replace = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $replace1);//delete empty lines
-function convert($replace)
+$csv_file = file_get_contents($_FILES['file']['tmp_name']);
+$delimiter = array("\t",",","|","\\","/",";");
+$tmp = str_replace($delimiter, $delimiter[1], $csv_file);// convert delimiter to ,
+$csv = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $tmp);//delete empty lines
+function convert($csv)
 {
   $out = [];
   array_map(function($ln) use(&$out) {
@@ -11,9 +11,9 @@ function convert($replace)
       $out[] = count($out) == 0
         ? "\t<tr class=\"rnkh_bkcolor\">\n\t\t<th>".implode("</th>\n\t\t<th>",explode(",",$ln))."</th>\n\t</tr>\n"
         : "\t<tr class=\"rnk_bkcolor\">\n\t\t<td class=\"rnk_font\">".implode("</td>\n\t\t<td class=\"rnk_font\">",explode(",",$ln))."</td>\n\t</tr>\n";
-    }, explode("\n",$replace));
+    }, explode("\n",$csv));
   return "\n<table cellspacing=1 class=\"line_color\">\n".implode('',$out)."</table>\n";
 }
  
-echo convert($replace);
+echo convert($csv);
 ?>
--- manual/auto_convert/index.php
+++ manual/auto_convert/index.php
@@ -1,15 +1,16 @@
- <?php include("/home/raz/public_html/password_protect.php"); ?>
+<?php include("/home/raz/public_html/password_protect.php"); ?>
+<!DOCTYPE html>
 <html class="no-js" lang="he" xml:lang="he" dir="rtl">
 <head>
 <title>Convert CSV to HTML Table</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<meta charset="utf-8"/>
+<meta charset="UTF-8"/>
 
 </head>
 <body>
 <div  style="font-size: 1.2em; margin: 40px auto 0 auto; width:400px;">
 <h3>Convert CSV to HTML Table</h3><br/>
-<form accept-charset="utf-8" action="converted.php" method="post" enctype="multipart/form-data">
+<form accept-charset="UTF-8" action="converted.php" method="post" enctype="multipart/form-data">
   <input type="file" name="file" style="color:red; font-size: 1.2em;"/><br/><br/>
 <br/>
   <input type="submit" value="Convert" style="font-size: 1.2em;"/>
