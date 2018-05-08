41e83735f4fd024953501b6af82bf1108d61850e
tnuatiming <tnuatiming@gmail.com>
2018-05-08 08:50:30 +0300

    update sw

--- service-worker20180507.js
+++ service-worker20180507.js
@@ -1,33 +1,32 @@
-var cacheName = 'tnuaTimingCache';
-var filesToCache = ['/images/logo16.png', '/style/global.css'];
+// HTML files: try the network first, then the cache.
+// Other files: try the cache first, then the network.
+// Both: cache a fresh version if possible.
+// (beware: the cache will grow and grow; there's no cleanup)
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        }).then(function() {
-            return self.skipWaiting();
-        })
-    );
+const cacheName = 'files';
+
+addEventListener('fetch',  fetchEvent => {
+  const request = fetchEvent.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  fetchEvent.respondWith(async function() {
+    const responseFromFetch = fetch(request);
+    fetchEvent.waitUntil(async function() {
+      const responseCopy = (await responseFromFetch).clone();
+      const myCache = await caches.open(cacheName);
+      await myCache.put(request, responseCopy);
+    }());
+    if (request.headers.get('Accept').includes('text/html')) {
+      try {
+        return await responseFromFetch;
+      }
+      catch(error) {
+        return caches.match(request);
+      }
+    } else {
+      const responseFromCache = await caches.match(request);
+      return responseFromCache || responseFromFetch;
+    }
+  }());
 });
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-      caches.keys().then(function(keyList) {
-          return Promise.all(keyList.map(function(key) {
-              if (key !== cacheName) {
-                  return caches.delete(key);
-              }
-          }));
-      })
-    );
-    return self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-}); 
