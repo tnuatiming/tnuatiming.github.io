404c32f73c321cf46ad0f59d919f51ce983c5904
tnuatiming <tnuatiming@gmail.com>
2020-07-05 16:16:53 +0300

    update hugo

--- gitup_descr.sh
+++ gitup_descr.sh
@@ -2,6 +2,6 @@
 read -e -p "Commit description: " desc
 desc=${desc:-# upload from local #}
 #cd $HOME/tnuatiming.github.io
-git add -A && git commit -m "$desc" && git push -u origin master
+git add -A && git commit -m "$desc" && git push -u origin hugo
 
 # upload from local #
